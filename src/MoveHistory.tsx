0d086c94bb2c90490b1f3152b214cb193047c92e
Abraham Hosch <hosch250@live.com>
2021-08-31 20:48:40 -0500

    Refactor move history

--- src/MoveHistory.tsx
+++ src/MoveHistory.tsx
@@ -10,30 +10,32 @@ function MoveHistory() {
       <fieldset>
         <legend className="visually-hidden">Move History</legend>
         {value.MoveHistory.map((m) => (
-          <div key={m.MoveNumber}>
-            <div style={{ display: 'inline-block', width: '2rem' }}>
+          <div className="row" key={m.MoveNumber}>
+            <div className="col-2 text-start">
               {m.MoveNumber}.
             </div>
             {m.BlackMove?.DisplayString ? (
-              <label style={{ display: 'inline-block', width: '5rem' }}>
+              <label className="col-5 text-start">
                 <input
+                  className="d-none"
                   value={m.BlackMove.DisplayString}
                   type="radio"
                   role="button"
                   name="move"
                 />
-                <span>{m.BlackMove.Move.length > 3 ? `${m.BlackMove.Move[0]}…${last(m.BlackMove.Move)}` : m.BlackMove.DisplayString}</span>
+                <span className="d-inline-block px-1">{m.BlackMove.Move.length > 3 ? `${m.BlackMove.Move[0]}…${last(m.BlackMove.Move)}` : m.BlackMove.DisplayString}</span>
               </label>
             ) : null}
             {m.WhiteMove?.DisplayString ? (
-              <label style={{ display: 'inline-block', width: '5rem' }}>
+              <label className="col-5 text-start">
                 <input
+                  className="d-none"
                   value={m.WhiteMove.DisplayString}
                   type="radio"
                   role="button"
                   name="move"
                 />
-                <span>{m.WhiteMove.Move.length > 3 ? `${m.WhiteMove.Move[0]}…${last(m.WhiteMove.Move)}` : m.WhiteMove.DisplayString}</span>
+                <span className="d-inline-block px-1">{m.WhiteMove.Move.length > 3 ? `${m.WhiteMove.Move[0]}…${last(m.WhiteMove.Move)}` : m.WhiteMove.DisplayString}</span>
               </label>
             ) : null}
           </div>
